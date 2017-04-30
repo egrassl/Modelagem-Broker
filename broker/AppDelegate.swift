bc89949fb296b55e113aad4607781fa2700547fe
Eric de Baere Grassl <coala@erics-macbook-pro.local>
2017-04-30 17:32:06 -0300

    Initial Commit

---
+++ broker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  broker
+//
+//  Created by Eric Grassl on 4/30/17.
+//  Copyright © 2017 egrassl. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ broker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  broker
+//
+//  Created by Eric Grassl on 4/30/17.
+//  Copyright © 2017 egrassl. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
